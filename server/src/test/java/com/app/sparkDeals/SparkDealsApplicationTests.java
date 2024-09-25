4df7a87338423e65d1b2d572aec5722c3fc84e47
Gayathri-Saravanan-ZSINT0184 <gayathri.saravanan@zucisystems.com>
2024-09-25 16:22:06 +0530

    Initial Commit
    Setup for backend

---
+++ server/src/test/java/com/app/sparkDeals/SparkDealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sparkDeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkDealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
